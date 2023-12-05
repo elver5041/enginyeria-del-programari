4253d296188c81a8177d4834fcd6b6b9d7d36ddb
Arnaldopower <116560232+Arnaldopower@users.noreply.github.com>
2023-12-05 12:29:15 +0100

    VotingOption enunciat

---
+++ Unit Testing/PracticaEng/src/data/java/data/VotingOption.java
@@ -0,0 +1,21 @@
+package data;
+
+final public class VotingOption {
+    private final String party;
+    public VotingOption (String option) { this.party = option; }
+    public String getParty () { return party; }
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VotingOption vO = (VotingOption) o;
+        return party.equals(niff.party);
+    }
+    @Override
+    public int hashCode () { return party.hashCode(); }
+    @Override
+    public String toString () {
+        return "Vote option {" + "party='" + party + '\'' + '}';
+    }
+
+}
