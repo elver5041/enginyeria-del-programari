794b441c6b07deea0502ae62f711a8aab71b7d67
elver <joelros2003@gmail.com>
2023-12-05 09:40:43 +0100

    ex3

---
+++ Unit Testing/Receipt/src/Receipt.java
@@ -0,0 +1,31 @@
+import exceptions.IsClosedException;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receipt {
+    List<Line> receipt = new ArrayList<>();
+    Boolean isReceiptClosed = false;
+    BigDecimal taxes = BigDecimal.ZERO;
+
+    public void addLine(BigDecimal pricePerUnit, int numUnits) throws IsClosedException {
+        if(isReceiptClosed) throw new IsClosedException("receipt already closed");
+        receipt.add(new Line(pricePerUnit,numUnits));
+    }
+
+    public void addTaxes(BigDecimal percent) throws IsClosedException {
+        if(isReceiptClosed) throw new IsClosedException("receipt already closed");
+        taxes = getTotal().multiply(percent);
+        isReceiptClosed = true;
+    }
+
+    public BigDecimal getTotal() {
+        BigDecimal total = BigDecimal.ZERO;
+        for(Line line : receipt){
+            total.add(line.totalCost());
+        }
+        total.add(taxes);
+        return total;
+    }
+}
