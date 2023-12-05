52028645703b667960c7a027c5f19758e069d5ff
oscarvdcr <73657935+oscarvdcr@users.noreply.github.com>
2023-12-05 11:16:45 +0100

    asdasdas

--- Unit Testing/Receipt/src/Receipt.java
+++ Unit Testing/Receipt/src/Receipt.java
@@ -1,12 +1,22 @@
-public class Receipt {
-    public void addLine(BigDecimal pricePerUnit, int numUnits) throws IsClosedException {
+import java.math.BigDecimal;
+import java.util.*;
 
+public class Receipt {
+    BigDecimal total = BigDecimal.ZERO;
+    Boolean isRecieptClosed = false;
+    List receipt = new ArrayList<>();
+    public void addLine(BigDecimal pricePerUnit, int numUnits) throws IsClosedException {
+        if(isRecieptClosed) throw new IsClosedException("Reciept Closed");
+        total=total.add(pricePerUnit.multiply(BigDecimal.valueOf(numUnits)));
+        receipt.add(new Pair(pricePerUnit, numUnits));
     }
     public void addTaxes(BigDecimal percent) throws IsClosedException {
-
+        if(isRecieptClosed) throw new IsClosedException("Reciept Closed");
+        total = total.add(total.multiply(percent));
+        isRecieptClosed = true;
     }
     public BigDecimal getTotal() {
-
-        return null;
+        return total;
     }
 }
+
