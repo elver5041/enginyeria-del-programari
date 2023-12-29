1174313d529d9e46e325ba305e4b4d125401990b
oscarvdcr <73657935+oscarvdcr@users.noreply.github.com>
2023-12-29 16:51:55 +0100

    Test1.0

--- Unit Testing/PracticaEng/Test/testData/testNif.java
+++ Unit Testing/PracticaEng/Test/testData/testNif.java
@@ -5,8 +5,7 @@ import exceptions.NotValidNifException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class testNif {
     @Test
@@ -21,11 +20,19 @@ public class testNif {
         assertTrue(actualMsg.contains(expectedMsg));
     }
     @Test
-    void nifLength() {
-        Exception exception0 = assertThrows(NullPointerException.class, () -> {
+    void nifEmpty() {
+        Exception exception0 = assertThrows(NotValidNifException.class, () -> {
             new Nif("");
         });
-        Exception exception1 = assertThrows(NotValidNifException.class, () -> {
+
+        String expectedMsg = "no te suficients caràcters";
+        String actualMsg0 = exception0.getMessage();
+
+        assertTrue(actualMsg0.contains(expectedMsg));
+    }
+    @Test
+    void nifWrongLength() {
+                Exception exception1 = assertThrows(NotValidNifException.class, () -> {
             new Nif("123456");
         });
         Exception exception2 = assertThrows(NotValidNifException.class, () -> {
@@ -33,36 +40,75 @@ public class testNif {
         });
 
         String expectedMsg = "no te suficients caràcters";
-        String actualMsg0 = exception0.getMessage();
         String actualMsg1 = exception1.getMessage();
         String actualMsg2 = exception2.getMessage();
 
-        assertTrue(actualMsg0.contains(expectedMsg));
         assertTrue(actualMsg1.contains(expectedMsg));
         assertTrue(actualMsg2.contains(expectedMsg));
     }
     @Test
-    void nifNumberFormat() {
+    void nifWrongNumberFormat() {
         Exception exception1 = assertThrows(NotValidNifException.class, () -> {
            new Nif("123abc78A");
         });
         Exception exception2 = assertThrows(NotValidNifException.class, () -> {
             new Nif("S12345678");
         });
+
         String expectedMsg = "8 primers caracters no son nombres";
         String actualMsg1 = exception1.getMessage();
         String actualMsg2 = exception2.getMessage();
+
         assertTrue(actualMsg1.contains(expectedMsg));
         assertTrue(actualMsg2.contains(expectedMsg));
     }
     @Test
-    void nifCorrectLetter() {
+    void nifLetterInvalid() {
         Exception exception1 = assertThrows(NotValidNifException.class, () -> {
-           new Nif("12312323A");
+           new Nif("12312323I");
         });
+        Exception exception2 = assertThrows(NotValidNifException.class, () -> {
+            new Nif("12312323U");
+        });
+        Exception exception3 = assertThrows(NotValidNifException.class, () -> {
+            new Nif("12312323O");
+        });
+
+        String expectedMsg = "nif invalid";
+        String actualMsg1 = exception1.getMessage();
+        String actualMsg2 = exception2.getMessage();
+        String actualMsg3 = exception3.getMessage();
+
+        assertTrue(actualMsg1.contains(expectedMsg));
+        assertTrue(actualMsg2.contains(expectedMsg));
+        assertTrue(actualMsg3.contains(expectedMsg));
     }
     @Test
-    void nifGood() {
+    void nifLetterIncorrect() {
+        Exception exception1 = assertThrows(NotValidNifException.class, () -> {
+            new Nif("12345678A");
+        });
+        Exception exception2 = assertThrows(NotValidNifException.class, () -> {
+            new Nif("56788765F");
+        });
 
+        String expectedMsg = "nif invalid";
+        String actualMsg1 = exception1.getMessage();
+        String actualMsg2 = exception2.getMessage();
+
+        assertTrue(actualMsg1.contains(expectedMsg));
+        assertTrue(actualMsg2.contains(expectedMsg));
+    }
+    @Test
+    void nifWellDone() throws NotValidNifException{
+        assertDoesNotThrow(()-> {
+            new Nif("12345678Z");
+        });
+        assertDoesNotThrow(() -> {
+            new Nif("65899632D");
+        });
+        assertDoesNotThrow(() -> {
+            new Nif("48255629L");
+        });
     }
 }
