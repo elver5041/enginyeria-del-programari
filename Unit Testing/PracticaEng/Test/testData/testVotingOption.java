7fc1d70a45065e57c4de2f2098d651c2b345126b
oscarvdcr <oscarvdcr@gmail.com>
2023-12-30 20:03:28 +0100

    Last data test update

--- Unit Testing/PracticaEng/Test/testData/testVotingOption.java
+++ Unit Testing/PracticaEng/Test/testData/testVotingOption.java
@@ -1,4 +1,51 @@
 package testData;
 
+import data.VotingOption;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class testVotingOption {
+    VotingOption votingOption1;
+    VotingOption votingOption2;
+    VotingOption votingOption3;
+    VotingOption votingOption4;
+    VotingOption votingOption5;
+
+    @BeforeEach
+    void createVO(){
+        votingOption1 = new VotingOption("Op1");
+        votingOption2 = new VotingOption("Op2");
+        votingOption3 = new VotingOption("Op1");
+        votingOption4 = new VotingOption("");
+        votingOption5 = new VotingOption(null);
+    }
+    @Test
+    void testGetParty() {
+        assertEquals("Op1", votingOption1.getParty());
+        assertNotEquals("Op3", votingOption3.getParty());
+        assertEquals("", votingOption4.getParty());
+        assertEquals(null, votingOption5.getParty());
+    }
+    @Test
+    void testEquals() {
+        assertTrue(votingOption1.equals(votingOption1));
+        assertTrue(votingOption1.equals(votingOption3));
+        assertFalse(votingOption1.equals(votingOption2));
+        assertFalse(votingOption4.equals(votingOption5));
+    }
+    @Test
+    void testHash() {
+        assertEquals(votingOption1.hashCode(), votingOption3.hashCode());
+        assertEquals(votingOption2.hashCode(), votingOption2.hashCode());
+        assertNotEquals(votingOption1.hashCode(), votingOption4.hashCode());
+        assertNotEquals(votingOption4.hashCode(), votingOption5.hashCode());
+    }
+    @Test
+    void testToString() {
+        assertEquals("Vote option {" + "party='" + "Op1" + '\'' + '}', votingOption1.toString());
+        assertEquals("Vote option {" + "party='" + "empty" + '\'' + '}', votingOption4.toString());
+        assertEquals("Vote option {" + "party='" + "null" + '\'' + '}', votingOption5.toString());
+    }
 }
